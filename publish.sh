6c6c33c7b07aae55f0fedff7ec491cdf6bc43dfe
Nathan Grubb <nate@evidence.com>
2017-09-01 11:07:30 -0700

    Fix publish script again

--- publish.sh
+++ publish.sh
@@ -3,11 +3,6 @@ render_template() {
   eval "echo \"$(cat $1)\""
 }
 
-if [ -z $1 ]; then
-  echo "Please specify pypi server to publish to"
-  exit 1
-fi
-
 repo="https://github.com/silent-snowman/git_bump_version"
 package_name=git_bump_version
 version=$(git_bump_version --dont_tag)
