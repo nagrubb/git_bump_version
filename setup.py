41c4ad6dc7cc0c128e9d1b0b4ba7d0df202d8843
Nathan Grubb <nate@evidence.com>
2017-09-01 10:43:28 -0700

    Update setup.py for 0.1.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'git_bump_version',
   packages = ['git_bump_version'], # this must be the same as the name above
-  version = '0.1.2',
+  version = '0.1.3',
   description = 'Automatically bumps version based on last tag and current branch',
   author = 'Nathan Grubb',
   author_email = 'mrnathangrubb@gmail.com',
-  url = 'https://github.com/silent-snowman/git-bump-version',
-  download_url = 'https://github.com/silent-snowman/git-bump-version/archive/0.1.2.tar.gz',
+  url = 'https://github.com/silent-snowman/git_bump_version',
+  download_url = 'https://github.com/silent-snowman/git_bump_version/archive/0.1.3.tar.gz',
   keywords = ['git', 'tag', 'version'],
   classifiers = [],
   entry_points={
