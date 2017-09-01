0e3e420bef4410e524ae8937857aa2f17a1f3f3e
Nathan Grubb <nate@evidence.com>
2017-09-01 10:55:11 -0700

    Modify publish script to handle multiple PyPI servers

--- publish.sh
+++ publish.sh
@@ -3,6 +3,11 @@ render_template() {
   eval "echo \"$(cat $1)\""
 }
 
+if [ -z $1 ]; then
+  echo "Please specify pypi server to publish to"
+  exit 1
+fi
+
 repo="https://github.com/silent-snowman/git_bump_version"
 package_name=git_bump_version
 version=$(git_bump_version --dont_tag)
@@ -19,5 +24,5 @@ git add setup.py
 git commit -m "Update setup.py for ${version}"
 git push -u origin
 git_bump_version
-python setup.py sdist upload -r pypitest
+python setup.py sdist upload -r $1 
 exit 0
