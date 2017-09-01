faef63d4501624c86f5b2654b4a247aa2a76018c
Nathan Grubb <nate@evidence.com>
2017-09-01 10:43:23 -0700

    Fix publish and template

--- publish.sh
+++ publish.sh
@@ -3,6 +3,7 @@ render_template() {
   eval "echo \"$(cat $1)\""
 }
 
+repo="https://github.com/silent-snowman/git_bump_version"
 package_name=git_bump_version
 version=$(git_bump_version --dont_tag)
 
