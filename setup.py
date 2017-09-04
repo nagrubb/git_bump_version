6c7b725df5a671f54ecc9689837ae6ead7996ca9
Nathan Grubb <mrnathangrubb@gmail.com>
2017-09-03 18:07:11 -0700

    Update setup.py for 0.1.14

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from distutils.core import setup
 setup(
   name = 'git_bump_version',
   packages = ['git_bump_version'], # this must be the same as the name above
-  version = '0.1.13',
+  version = '0.1.14',
   description = 'Automatically bumps version based on last tag and current branch',
   author = 'Nathan Grubb',
   author_email = 'mrnathangrubb@gmail.com',
   url = 'https://github.com/silent-snowman/git_bump_version',
-  download_url = 'https://github.com/silent-snowman/git_bump_version/archive/0.1.13.tar.gz',
+  download_url = 'https://github.com/silent-snowman/git_bump_version/archive/0.1.14.tar.gz',
   keywords = ['git', 'tag', 'version'],
   classifiers = [],
   entry_points={
     'console_scripts' : [
-      'git_bump_version = git_bump_version.__main__:main'
+      'git_bump_version = git_bump_version.__init__:main'
     ]},
   install_requires=[
     'GitPython',
