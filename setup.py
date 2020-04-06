ccaf0195b3cea6d0b6a836807a2652332420fad3
Nathan Grubb <ngrubb@axon.com>
2020-04-06 20:34:52 +0700

    Cleanup

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-from distutils.core import setup
+import sys
+from setuptools import setup
+
 setup(
   name = 'git_bump_version',
   packages = ['git_bump_version'], # this must be the same as the name above
-  version = '0.1.19',
+  use_scm_version=True
   description = 'Automatically bumps version based on last tag and current branch',
   author = 'Nathan Grubb',
   author_email = 'mrnathangrubb@gmail.com',
   url = 'https://github.com/silent-snowman/git_bump_version',
-  download_url = 'https://github.com/silent-snowman/git_bump_version/archive/0.1.19.tar.gz',
   keywords = ['git', 'tag', 'version'],
-  classifiers = [],
+  setup_requires=[
+    'setuptools_scm',
+  ],
   entry_points={
     'console_scripts' : [
       'git_bump_version = git_bump_version.__init__:main'
